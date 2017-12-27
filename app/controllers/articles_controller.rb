a0577d381387808bf596085b5e7941eaca65b853
Alex Schaeffer <soccerboy1912@outlook.com>
2017-12-27 18:45:46 +0000

    Inital commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,73 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
+  # GET /articles.json
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  # GET /articles/1.json
+  def show
+    @article = Article.find(params[:id])
+  end
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # POST /articles
+  # POST /articles.json
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:notice] = "Article was successfully created."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    
+  end
+
+  # PATCH/PUT /articles/1
+  # PATCH/PUT /articles/1.json
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params )
+      flash[:notice] = "Article was updated successfuly!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  # DELETE /articles/1
+  # DELETE /articles/1.json
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+    
+    
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+end
