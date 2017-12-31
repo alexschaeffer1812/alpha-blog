d9464f83d7ed3e7cbdd047eca28adbad38279eb1
Alex Schaeffer <soccerboy1912@outlook.com>
2017-12-31 03:49:05 +0000

    Styled alert messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article was successfully created."
+      flash[:success] = "Article was successfully created."
       redirect_to article_path(@article)
     else
       render 'new'
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
    
     
     if @article.update(article_params )
-      flash[:notice] = "Article was updated successfuly!"
+      flash[:success] = "Article was updated successfuly!"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Article was deleted"
+    flash[:danger] = "Article was deleted"
     redirect_to articles_path
   end
 
