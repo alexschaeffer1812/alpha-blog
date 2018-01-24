a8c5a158974f8da8ba44ed2a9fda09f84f13e0a6
Alex Schaeffer <soccerboy1912@outlook.com>
2018-01-24 02:47:37 +0000

    added authentication methods

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :users, except: [:new]
  
   # The priority is based upon order of creation: first created -> highest priority.
